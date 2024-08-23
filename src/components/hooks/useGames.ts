3cb1ebf457f07b23aebe79d01f5eeb74c93e1d21
Leela Sai Prasanth <59244101+leelasaiprasanth@users.noreply.github.com>
2024-08-23 16:27:54 +0530

    Implement Search

--- src/components/hooks/useGames.ts
+++ src/components/hooks/useGames.ts
@@ -23,6 +23,7 @@ const useGames = (gameQuery: GameQuery) =>
         genres: gameQuery.genre?.id,
         platfroms: gameQuery.platform?.id,
         ordering: gameQuery.sortOrder,
+        search: gameQuery.searchText,
       },
     },
     [gameQuery]
