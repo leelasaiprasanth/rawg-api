51438db65a0b9d44b175a3152b35634d881009e8
Leela Sai Prasanth <59244101+leelasaiprasanth@users.noreply.github.com>
2024-08-23 14:37:18 +0530

    Display Genres

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -10,10 +10,10 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card width="300px" borderRadius={10} overflow="hidden">
+    <Card borderRadius={10} overflow="hidden">
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
-        <Heading fontSize="2l">{game.name}</Heading>
+        <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justifyContent="space-between">
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
