cb72716bc16d20ea7ccfb93d5a5820887541b719
Leela Sai Prasanth <59244101+leelasaiprasanth@users.noreply.github.com>
2024-08-26 23:58:09 +0530

    Refactoring Entities

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -2,14 +2,9 @@ import { useQuery } from "@tanstack/react-query";
 import ms from "ms";
 import platforms from "../data/platforms";
 import APIClinet, { FetchResponse } from "../services/api-client";
+import { Platform } from "../entities/Platform";
 
 const apiClient = new APIClinet<Platform>("/platforms/list/parents");
-
-export interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
 
 const usePlatfroms = () =>
   useQuery<FetchResponse<Platform>, Error>({
